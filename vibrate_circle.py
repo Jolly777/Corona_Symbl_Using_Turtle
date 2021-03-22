2691cf92aeae4b50d4b6e2737e346d47479495b2
Ganesh Acharya K <50414101+Jolly777@users.noreply.github.com>
2021-03-22 11:41:44 +0530

    Add files via upload

---
+++ vibrate_circle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#python_vibrate_circle
+import turtle  
+# Creating turtle  
+t = turtle.Turtle()  
+s = turtle.Screen()  
+s.bgcolor("black")  
+t.pencolor("green")  
+t.shape("turtle")
+  
+a = 0  
+b = 0  
+t.speed(0)  
+t.penup()  
+t.goto(0,200)  
+t.pendown()  
+while(True):  
+    t.forward(a)  
+    t.right(b)  
+    a+=3  
+    b+=1  
+    if b == 210:  
+        break 
+t.penup()
+t.goto(20,0)
+t.pendown()
+t.pencolor("red")
+t.write("CORONA-VIRUS", align="CENTER", font=("Dades", 35))
+t.penup()
+turtle.done()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
